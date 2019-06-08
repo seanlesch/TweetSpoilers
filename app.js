ef238a6d34ba567893e07108bd25b023337064d5
seanlesch <38013435+seanlesch@users.noreply.github.com>
2019-06-07 17:55:00 -0700

    Added getTweets function, fixed front end loop

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var Twitter = require('twitter');
 var request = require('request');
 var config = require('./config.js');
+var querystring = require('querystring');
 var bodyParser = require('body-parser');
 var express = require('express');
 var T = new Twitter(config);
@@ -8,15 +9,8 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);;
 
-var tweets = {
 
-}
 
-var params = {
-    q: 'iron man dies -spoiler',
-    count: 1,
- 
-}
 
 server.listen(3000);
 console.log("Server listening at: 3000");
@@ -29,28 +23,47 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
+    var params = {
+        q: 'iron man dies -spoiler',
+        count: 25,
+        result_type: 'mixed'
+     
+    }
     //socket.emit('welcome', { data: 'welcome'});
     T.get('search/tweets', params, function(err, data, response){
         if(!err){
-            var results = []
-            /*for(let i = 0; i < data.results.length; i++){
-                results.push({
-                    name: data.results[i].user.screen_name,
-                    text: data.results[i].text,
-                    id: data.results[i].id_str});
-                
-                console.log(data.results[i].created_at)
-                console.log(data.results[i].user.screen_name)
-                console.log(data.results[i].text)
-    
+            socket.emit('tweet', { data: data });
+            
+           /* if (data.search_metadata.next_results) {
+                getTweets(data.search_metadata.next_results, socket, function () {
+                    console.log('No more results');
+                });
             }*/
-            socket.emit('tweet', {data: data});
-            //console.log(results);
+            
         } else {
             console.log(err);
         }
     });
     
+    
 });
 
+function getTweets(url, socket, callback) {
+    params = querystring.parse(url);
+    T.get('https://api.twitter.com/1.1/search/tweets.json'+url, params, function (err, data, response) {
+        if (!err && response.statusCode === 200) {
+            socket.emit('tweet', { data: data });
+            if (data.search_metadata.next_results) {
+                getTweets(data.search_metadata.next_results, callback);
+            }
+            else {
+                callback();
+            }
+        }
+        else {
+            console.log(err);
+        }
+    })
+    
+}
